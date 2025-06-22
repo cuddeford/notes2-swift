01a74a639e1ad6063abf09b4c5ea9491ca9ddbab
Lucio Cuddeford <cuddeford@me.com>
2025-06-22 11:56:17 +0100

    Initial Commit

---
+++ notes2/notes2App.swift
@@ -0,0 +1,17 @@
+//
+//  notes2App.swift
+//  notes2
+//
+//  Created by Lucio Cuddeford on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct notes2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
