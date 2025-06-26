4390624f4eb01dd141b5009d9a53f475d7e176ab
Lucio Cuddeford <cuddeford@me.com>
2025-06-26 17:31:26 +0100

    Remove the Done button as the toolbar is now hidden

--- notes2/ContentView.swift
+++ notes2/ContentView.swift
@@ -264,19 +264,20 @@ struct NoteView: View {
                 }
             }
         }
-        .toolbar {
-            if keyboard.isKeyboardVisible {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        UIApplication.shared.sendAction(
-                            #selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil,
-                        )
-                    }) {
-                        Text("Done")
-                    }
-                }
-            }
-        }
+        // .toolbar {
+        //     if keyboard.isKeyboardVisible {
+        //         ToolbarItem(placement: .navigationBarTrailing) {
+        //             Button(action: {
+        //                 // hide the keyboard
+        //                 UIApplication.shared.sendAction(
+        //                     #selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil,
+        //                 )
+        //             }) {
+        //                 Text("Done")
+        //             }
+        //         }
+        //     }
+        // }
         .ignoresSafeArea()
         .toolbar(.hidden, for: .navigationBar)
         .onAppear {
