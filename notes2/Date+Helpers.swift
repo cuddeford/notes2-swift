cd37043bd214cf685f4bd25370d6569b17e0f6a7
Lucio Cuddeford <cuddeford@me.com>
2025-06-26 17:11:13 +0100

    Reformat date

--- notes2/Date+Helpers.swift
+++ notes2/Date+Helpers.swift
@@ -3,8 +3,24 @@ import Foundation
 extension Date {
     func formattedDate() -> String {
         let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .none
+        let calendar = Calendar.current
+        let day = calendar.component(.day, from: self)
+
+        let suffix: String
+        switch day {
+        case 1, 21, 31: suffix = "st"
+        case 2, 22: suffix = "nd"
+        case 3, 23: suffix = "rd"
+        default: suffix = "th"
+        }
+
+        if calendar.isDate(self, equalTo: Date(), toGranularity: .year) {
+            // Same year, exclude year
+            formatter.dateFormat = "d'\(suffix)' MMMM"
+        } else {
+            // Different year, include year
+            formatter.dateFormat = "d'\(suffix)' MMMM yy"
+        }
         return formatter.string(from: self)
     }
 
