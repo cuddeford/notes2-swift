3b734f464f94a2b91cca11e4320954ac0b4ceb95
Lucio Cuddeford <cuddeford@me.com>
2025-07-06 19:06:13 +0100

    Fix ruled lines

--- notes2/RuledView.swift
+++ notes2/RuledView.swift
@@ -18,18 +18,38 @@ class RuledView: UIView {
     override func draw(_ rect: CGRect) {
         guard let context = UIGraphicsGetCurrentContext(), let textView = textView else { return }
 
-        let lineColor = UIColor.lightGray.withAlphaComponent(0.7)
+        let lineColor = UIColor.lightGray.withAlphaComponent(1.0)
         context.setStrokeColor(lineColor.cgColor)
         context.setLineWidth(1.0) // As you requested
 
         let layoutManager = textView.layoutManager
+        let textStorage = textView.textStorage
         let textContainer = textView.textContainer
-        var glyphIndex = 0
 
+        var glyphIndex = 0
         while glyphIndex < layoutManager.numberOfGlyphs {
             var glyphRange = NSRange()
             let lineRect = layoutManager.lineFragmentRect(forGlyphAt: glyphIndex, effectiveRange: &glyphRange)
-            let lineY = lineRect.origin.y + lineRect.height
+            let characterRange = layoutManager.characterRange(forGlyphRange: glyphRange, actualGlyphRange: nil)
+
+            var lineY = lineRect.origin.y + lineRect.height
+
+            // Adjust for paragraph spacing if this is the last line of a paragraph
+            if let paragraphStyle = textStorage.attribute(.paragraphStyle, at: characterRange.location, effectiveRange: nil) as? NSParagraphStyle {
+                let paragraphRange = (textStorage.string as NSString).paragraphRange(for: characterRange)
+                let isLastLineOfParagraph = NSMaxRange(characterRange) == NSMaxRange(paragraphRange)
+
+                if isLastLineOfParagraph {
+                    lineY -= paragraphStyle.paragraphSpacing
+                }
+            }
+
+            // Adjust for the "lines too high" issue.
+            // This offset moves the line down slightly to be truly "underneath" the text.
+            // The exact value might need to be fine-tuned.
+            let verticalOffset: CGFloat = 15.0
+
+            lineY += verticalOffset
 
             // Draw the line from edge to edge of the view's bounds.
             context.move(to: CGPoint(x: self.bounds.minX, y: lineY))
