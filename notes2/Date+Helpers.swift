6d51aca19b8d456c48cb4f09e1d9d500dec07810
Lucio Cuddeford <cuddeford@me.com>
2025-07-24 15:46:37 +0100

    Fix relative date format

--- notes2/Date+Helpers.swift
+++ notes2/Date+Helpers.swift
@@ -26,24 +26,26 @@ extension Date {
 
     func relativeDate() -> String {
         let calendar = Calendar.current
-        if calendar.isDateInToday(self) {
-            return "Today"
-        }
-        if calendar.isDateInYesterday(self) {
-            return "Yesterday"
-        }
+        if calendar.isDateInToday(self) { return "Today" }
+        if calendar.isDateInYesterday(self) { return "Yesterday" }
 
         let now = Date()
-        let startOfWeek = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: now))!
+        let startOfToday = calendar.startOfDay(for: now)
+        let startOfDate = calendar.startOfDay(for: self)
 
-        if self >= startOfWeek {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "EEEE"
+        guard let daysDifference = calendar.dateComponents([.day], from: startOfDate, to: startOfToday).day else {
+            return ""
+        }
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE"
+
+        if daysDifference > 1 && daysDifference < 7 {
             return formatter.string(from: self)
-        } else {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "EEEE"
+        } else if daysDifference >= 7 && daysDifference < 14 {
             return "Last \(formatter.string(from: self))"
         }
+
+        return ""
     }
 }
